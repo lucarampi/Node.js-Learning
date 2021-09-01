96bb2f08d765c44f7cb91aac0363d55494c38681
Luca Alfaro Rampinelli <luca.alfaro.rampinelli@gmail.com>
2021-09-01 17:52:07 -0300

    5 - Passing parameters

---
+++ 5 - Passing parameters/client.js
@@ -0,0 +1,6 @@
+const wellcome = require('./passingParameters')
+console.log(wellcome('Luca','Everton','Dani','Zé','Ingon'))
+
+//or
+// const wellcome = require('./passingParameters')('Luca','Everton','Dani','Zé','Ingon')
+// console.log(wellcome)
