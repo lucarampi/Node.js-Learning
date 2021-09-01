5059520816755185ea3592b4537cd80cc9ab161a
Luca Alfaro Rampinelli <luca.alfaro.rampinelli@gmail.com>
2021-09-01 09:44:18 -0300

    fix

--- 2 - SC Hello Node (with Express)/hellonodeExpress.js
+++ 2 - SC Hello Node (with Express)/hellonodeExpress.js
@@ -2,8 +2,11 @@ const express = require("express");// imports express module
 const app = express()
 const port = 8000 //define the port we will use
 
-app.get('/', (req, res) => {
+app.get('/test', (req, res) => {
     res.end("Hello Node.js + Express!")
+});
+app.get('/', (req, res) => {
+    res.end("nnone")
 });
 
 app.listen(port, () => {
